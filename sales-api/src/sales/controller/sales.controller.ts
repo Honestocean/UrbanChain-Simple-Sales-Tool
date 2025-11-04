670fd9b6d7cc0ea74fe6baea33749aa515b0e12f
Simon Taylor <simon.taylor@tunstall.com>
2025-11-04 15:36:10 +0000

    added validation pipes, dto now provides validation for create sales

--- sales-api/src/sales/controller/sales.controller.ts
+++ sales-api/src/sales/controller/sales.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SalesService } from '../service/sales.service';
+import { CreateSaleDto } from './dto/create-sale.dto';
 
 @Controller('sales')
 export class SalesController {
   constructor(private salesService: SalesService) {}
 
   @Post()
-  async createSale(@Body() createSaleDto: any): Promise<any> {
+  async createSale(@Body() createSaleDto: CreateSaleDto): Promise<any> {
     return this.salesService.createSale(createSaleDto);
   }
 }
