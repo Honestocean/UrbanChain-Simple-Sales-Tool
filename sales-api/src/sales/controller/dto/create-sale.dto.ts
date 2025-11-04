91a129a75b6a49d0f0e55fe56fa3cbdf048e389f
Simon Taylor <simon.taylor@tunstall.com>
2025-11-04 15:19:08 +0000

    added sale.json for the data shape and make matching dto

--- sales-api/src/sales/controller/dto/create-sale.dto.ts
+++ sales-api/src/sales/controller/dto/create-sale.dto.ts
@@ -1,6 +1,28 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsDateString, IsEmail, IsIn, IsNotEmpty } from 'class-validator';
 
 export class CreateSaleDto {
   @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
+  customerName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  mpans: string[];
+
+  @IsNotEmpty()
+  @IsDateString()
+  contractStartDate: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  contractEndDate: string;
+
+  @IsNotEmpty()
+  @IsIn(['active', 'inactive', 'pending', 'cancelled'])
+  status: string;
 }
